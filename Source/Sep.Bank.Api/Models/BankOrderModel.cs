50fa0bcc0bc89bac65efa4845c709a8b908a8c54
sdmilos91 <sdmilos91@gmail.com>
2016-01-25 20:59:52 +0100

    Init commit

---
+++ Source/Sep.Bank.Api/Models/BankOrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sep.Bank.Api.Models
+{
+    public class BankOrderModel
+    {
+        public long PaymentID { get; set; }
+        public string CardPAN { get; set; }
+        public string CardSecurityCode { get; set; }
+        public string CardHolderName { get; set; }
+        public DateTime CardValidThru { get; set; }
+    }
+}
---
+++ Source/Sep.Bank.Api/Models/MerchantRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sep.Bank.Api.Models
+{
+    public class MerchantRequestModel
+    {
+        public string MerchantID { get; set; }
+        public string MerchantPassword { get; set; }
+        public decimal Amount { get; set; }
+        public long MerchantOrderID { get; set; }
+        public DateTime MerchantTimestamp { get; set; }
+        public string ErrorUrl { get; set; }
+
+    }
+}
